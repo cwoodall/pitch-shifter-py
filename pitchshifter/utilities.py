d2a15b5e7e7088ad45bf0d07b5f4e775ef65d47f
Christopher Woodall <chris.j.woodall@gmail.com>
2015-07-07 08:48:28 -0400

    Added tests for numpy arrays, also added complex_cartesianToPolar function to utilities library (and tests for it)

--- pitchshifter/utilities.py
+++ pitchshifter/utilities.py
@@ -9,6 +9,26 @@ def complex_polarToCartesian(r, theta):
     representation.
 
     Can be used with a numpy array allowing for block conversions
+
+    Example Usage:         
+    
+    results = complex_polarToCartesian(1.4142, 0.7854)
+
+    results approx. 1+1j 
     """
     return r * np.exp(theta*1j)
 
+def complex_cartesianToPolar(x):
+    """
+    Convert a cartesian representaiton of a complex number to a polar
+    representation.
+
+    Can be used with a numpy array allowing for block conversions
+
+    Example Usage:         
+    
+    results = complex_cartesianToPolar(1 + 1j)
+
+    results approx. (1.4142, 0.7854)
+    """
+    return (np.abs(x), np.angle(x))
--- test/__init__.py
+++ test/__init__.py
@@ -1,6 +1,7 @@
 #!python
 
 from pitchshifter import *
+import numpy as np
 
 class TestUtilities(object):
     def test_complex_polarToCartesian_correctResults(self):
@@ -12,7 +13,35 @@ class TestUtilities(object):
         assert abs(results.real - expected_results.real) < .01
         assert abs(expected_results.imag - results.imag) < .01
 
-class TestSTFT(object):
-    def test_stft_correct_results(self):
-        assert False
+    def test_complex_cartesianToPolar_correctResults(self):
+        results = complex_cartesianToPolar(1+1j)
+        
+        expected_results = (1.4142, .7854)
+        assert abs(results[0] - expected_results[0]) < .001
+        assert abs(results[1] - expected_results[1]) < .001
+
+    def test_complex_polarToCartesian_arrayCorrectResults(self):
+        rs = np.asarray([1.4142 for i in range(10)])
+        thetas = np.asarray([0.7854 for i in range(10)])
+        
+        results = complex_polarToCartesian(rs, thetas)
+        
+        expected_results = 1 + 1j
+        for result in results:
+            assert abs(result.real - expected_results.real) < .01
+            assert abs(expected_results.imag - result.imag) < .01
+
+    def test_complex_cartesianToPolar_arrayCorrectResults(self):
+        rs, thetas= complex_cartesianToPolar(np.asarray([1+1j for i in range(10)]))
+
+        expected_results = (1.4142, .7854)
+        for r in rs:
+            assert abs(r - expected_results[0]) < .001
+
+        for theta in thetas:
+            assert abs(theta - expected_results[1]) < .001
+    
+#class TestSTFT(object):
+#    def test_stft_correct_results(self):
+#        assert False
 
